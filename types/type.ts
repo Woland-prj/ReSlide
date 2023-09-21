1a243ce1a9a20ac3b0f7b5000364dc13d2d8fb77
unknown <denkist555@gmail.com>
2023-09-21 15:11:42 +0300

    Добавление типа TText и TFormatting

---
+++ types/type.ts
@@ -0,0 +1,11 @@
+type TText = {
+    Value: string;
+    Formatting: TFormatting
+}
+type TFormatting = {
+    Italics: boolean;
+    Bold: boolean;
+    Underline: boolean;
+    FontFamily: string;
+    Color: string;
+}
