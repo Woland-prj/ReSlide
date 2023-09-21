d04d09dc49f73ae08a003345af320cc9a319afbb
Woland-prj <nzimin93@gmail.com>
2023-09-21 15:16:25 +0300

    Adding a TypeSlideObject

---
+++ types/type.ts
@@ -0,0 +1,16 @@
+type TSlideObject {
+	type: TText | TVector | TImage
+	size:TSlideObjectSize
+	cords: TSlideObjectCords
+	rotationAngel: number
+}
+
+type TSlideObjectSize {
+	width: number
+	height: number
+}
+
+type TSlideObjectCords {
+	x: number
+	y: number
+}
