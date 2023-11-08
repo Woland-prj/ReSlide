24a6aecc44b283f23e9c45aab4ce4308ff68c655
unknown <denkist555@gmail.com>
2023-11-08 18:44:55 +0300

    Коммит

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,14 @@ import { doc } from '@/data/data_max'
 import TopMenu from '@ui/app_menu/top_menu/TopMenu'
 import { FC } from 'react'
 import { SlidePreview } from '@ui/slidebar/SlidePreview'
+import Slide from '@slide/Slide'
 
 const App: FC = () => {
   return (
     <>
       <TopMenu presentationName={doc.name} />
       <SlidePreview slides={doc.slides}></SlidePreview>
+      <Slide slide={doc.slides[1]}></Slide>
     </>
   )
 }
