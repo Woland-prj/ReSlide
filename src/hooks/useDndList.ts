5fa2d7737400de7ee113afbf77410d43acc46ec6
Woland-prj <nzimin93@gmail.com>
2023-11-11 12:14:41 +0300

    drag and drop hook

--- src/hooks/useDndList.ts
+++ src/hooks/useDndList.ts
@@ -1,47 +1,60 @@
-// import { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 
-// type TDraggedItemInfo = {
-//   element: HTMLDivElement
-//   startY: number
-// }
+type TDraggedItemInfo = {
+  element: HTMLDivElement
+  startY: number
+}
 
-// const useDndList = () => {
-//   const containerRef = useRef<HTMLDivElement>(null)
-//   const itemsRef = useRef<HTMLDivElement[]>([])
-//   const dragItemInfoRef = useRef<TDraggedItemInfo | null>(null)
-//   useEffect(() => {
-//     const unsubscribes = itemsRef.current.map(itemElement => {
-//       const onMouseDown = (e: MouseEvent) => {
-//         dragItemInfoRef.current = {
-//           element: itemElement,
-//           startY: e.clientY,
-//         }
-//       }
-//       itemElement.addEventListener('mousedown', onMouseDown)
+type TSubscription = {
+  element: HTMLDivElement
+  subscribers: ((e: MouseEvent) => void)[]
+}
 
-//       const onMouseMove = (e: MouseEvent) => {
-//         if (itemElement === dragItemInfoRef.current.element) {
-//           itemElement.style.position = 'relative'
-//           itemElement.style.top = e.clientY + 'px'
-//         }
-//       }
-//       itemElement.addEventListener('mousemove', onMouseMove)
-//     })
+const useDndList = () => {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const itemsRef = useRef<HTMLDivElement[]>([])
+  const dragItemInfoRef = useRef<TDraggedItemInfo | null>(null)
+  useEffect(() => {
+    const unsubscribes: TSubscription[] = itemsRef.current.map(itemElement => {
+      const onMouseDown = (e: MouseEvent) => {
+        dragItemInfoRef.current = {
+          element: itemElement,
+          startY: e.clientY,
+        }
+      }
+      itemElement.addEventListener('mousedown', onMouseDown)
 
-//     const unsubscribe = () => {}
+      const onMouseMove = (e: MouseEvent) => {
+        if (itemElement === dragItemInfoRef.current?.element) {
+          itemElement.style.position = 'relative'
+          itemElement.style.top = e.clientY + 'px'
+        }
+      }
+      itemElement.addEventListener('mousemove', onMouseMove)
 
-//     return () => {
-//       unsubscribes.forEach(unsubscribe => unsubscribe(unsubscribe))
-//     }
-//   }, [])
+      const onMouseUp = (e: MouseEvent) => {
+        if (itemElement === dragItemInfoRef.current?.element)
+          dragItemInfoRef.current = null
+      }
+      itemElement.addEventListener('mouseup', onMouseUp)
+      return {
+        element: itemElement,
+        subscribers: [onMouseDown, onMouseMove, onMouseUp],
+      }
+    })
 
-//   useEffect(() => {
-//     const draggedItem = draggedItemRef.current
-//     if (draggedItem) {
-//     }
-//   })
+    const unsubscribe = (action: TSubscription) => {
+      action.element.removeEventListener('mousedown', action.subscribers[0])
+      action.element.removeEventListener('mousemove', action.subscribers[1])
+      action.element.removeEventListener('mouseup', action.subscribers[2])
+    }
 
-//   return { containerRef, itemsRef }
-// }
+    return () => {
+      unsubscribes.forEach(action => unsubscribe(action))
+    }
+  }, [])
 
-// export default useDndList
+  return { containerRef, itemsRef }
+}
+
+export default useDndList
