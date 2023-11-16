4eab6783f91efaf17881664ec01510e73e68094d
unknown <denkist555@gmail.com>
2023-11-16 15:06:22 +0300

    SubMenu.module.css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,6 @@
 import { doc } from '@/data/data_max'
 import TopMenu from '@ui/app_menu/top_menu/TopMenu'
-
 import Slide from '@slide/Slide'
-
-// Denis
 import { FC } from 'react'
 import { SlidePreview } from '@ui/slidebar/SlidePreview'
 import styles from './App.module.css'
