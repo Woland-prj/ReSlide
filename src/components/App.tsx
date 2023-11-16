5d2f98945aabfa6fc0165bab01a0cb769ff89601
Woland-prj <nzimin93@gmail.com>
2023-11-16 15:39:13 +0300

    немного лэйаута

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,17 +5,13 @@ import Slide from '@slide/Slide'
 
 // Denis
 import { FC } from 'react'
-import { SlidePreview } from '@ui/slidebar/SlidePreview'
-import styles from './App.module.css'
+import Layout from '@/components/Layout/Layout'
 
 const App: FC = () => {
   return (
     <>
       <TopMenu presentationName={doc.name} />
-      <div className={styles.SlideContainer}>
-        <SlidePreview slides={doc.slides}></SlidePreview>
-        <Slide slide={doc.slides[1]} editable={true}></Slide>
-      </div>
+      <Layout></Layout>
     </>
   )
 }
