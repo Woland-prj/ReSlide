2a1065c8cefded94902bcd0f750a32eea3374e4c
unknown <denkist555@gmail.com>
2023-11-16 14:28:41 +0300

    TopMenu.module.css

--- src/components/ui/app_menu/top_menu/TopMenu.tsx
+++ src/components/ui/app_menu/top_menu/TopMenu.tsx
@@ -2,6 +2,9 @@ import MenuButtons from '@ui/app_menu/top_menu/MenuButtons'
 import MenuLogo from '@ui/app_menu/top_menu/MenuLogo'
 import PresentationName from '@ui/app_menu/top_menu/PresentationName'
 import { FC } from 'react'
+import { ImageButtons } from './ImageButtons'
+import styles from './TopMenu.module.css'
+
 type TopMenuProps = {
   presentationName: string
 }
@@ -9,8 +12,10 @@ type TopMenuProps = {
 const TopMenu: FC<TopMenuProps> = ({ presentationName }) => {
   return (
     <>
-      <MenuLogo />
-      <PresentationName name={presentationName}></PresentationName>
+      <div className={styles.LogoAndPresentationName}>
+        <MenuLogo />
+        <PresentationName name={presentationName}></PresentationName>
+      </div>
       <MenuButtons />
       <ImageButtons />
     </>
