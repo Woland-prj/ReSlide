2cfec98130f6cc2e4fa8fbe7965752b85680916a
Woland-prj <nzimin93@gmail.com>
2023-11-29 20:23:15 +0300

    dnd

--- src/hooks/useDndList.ts
+++ src/hooks/useDndList.ts
@@ -1,62 +1,28 @@
-// import { useEffect, useRef } from 'react'
-// import { isElement } from 'react-dom/test-utils'
-//
-// type TDraggedItemInfo = {
-//   element: HTMLDivElement
-//   startY: number
-// }
-//
-// type TSubscription = {
-//   element: HTMLDivElement
-//   subscribers: ((e: MouseEvent) => void)[]
-// }
-//
-// const useDndList = () => {
-//   const containerRef = useRef<HTMLDivElement>(null)
-//   const itemsRef = useRef<HTMLDivElement[]>([])
-//   const dragItemInfoRef = useRef<TDraggedItemInfo | null>(null)
-//   useEffect(() => {
-//     //const exItems: HTMLDivElement[] = itemsRef.current.filter(item => item != null)
-//     const unsubscribes: TSubscription[] = itemsRef.current.map(itemElement => {
-//       const onMouseDown = (e: MouseEvent) => {
-//         dragItemInfoRef.current = {
-//           element: itemElement,
-//           startY: e.clientY,
-//         }
-//       }
-//       itemElement.addEventListener('mousedown', onMouseDown)
-//
-//       const onMouseMove = (e: MouseEvent) => {
-//         if (itemElement === dragItemInfoRef.current?.element) {
-//           itemElement.style.position = 'relative'
-//           itemElement.style.top = e.clientY + 'px'
-//         }
-//       }
-//       itemElement.addEventListener('mousemove', onMouseMove)
-//
-//       const onMouseUp = (e: MouseEvent) => {
-//         if (itemElement === dragItemInfoRef.current?.element)
-//           dragItemInfoRef.current = null
-//       }
-//       itemElement.addEventListener('mouseup', onMouseUp)
-//       return {
-//         element: itemElement,
-//         subscribers: [onMouseDown, onMouseMove, onMouseUp],
-//       }
-//     })
-//
-//     const unsubscribe = (action: TSubscription) => {
-//       action.element.removeEventListener('mousedown', action.subscribers[0])
-//       action.element.removeEventListener('mousemove', action.subscribers[1])
-//       action.element.removeEventListener('mouseup', action.subscribers[2])
-//     }
-//
-//     return () => {
-//       unsubscribes.forEach(action => unsubscribe(action))
-//     }
-//   }, [])
-//
-//   return { containerRef, itemsRef }
-// }
-//
-// export default useDndList
+import { RefObject, useCallback, useEffect, useRef } from 'react'
+import { TCoords } from '@/types/type'
+
+type TItemInfo = {
+  elementRef: RefObject<HTMLDivElement>
+  startCoords: TCoords
+}
+
+const useDnd = () => {
+  const itemsRef = useRef<TItemInfo[]>([])
+
+  const registerDndItem = () =>
+    useCallback((index: number, info: TItemInfo) => {
+      const item: TItemInfo = {
+        ...info,
+        startCoords: {
+          x: 0,
+          y: 0,
+        },
+      }
+
+      itemsRef.current[index] = item
+
+      const onDrag
+    }, [])
+}
+
+export default useDnd
