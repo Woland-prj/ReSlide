8537e7ac4f08ecc53c2d61844ff86781b46d170f
Woland-prj <nzimin93@gmail.com>
2023-12-02 20:13:31 +0300

    попытка в SubMenu

--- src/components/ui/app_menu/top_menu/SubMenu.tsx
+++ src/components/ui/app_menu/top_menu/SubMenu.tsx
@@ -1,16 +1,23 @@
 import { TContextButton } from '@/types/menu_buttons.type'
-import { FC } from 'react'
+import { Dispatch, FC, SetStateAction, useEffect } from 'react'
 import styles from './SubMenu.module.css'
 import { SubMenuButton } from './SubMenuButton'
 
 type TSubMenuProps = {
   buttons: TContextButton[]
+  setIsMenuOpen: Dispatch<SetStateAction<boolean>>
 }
 
-const SubMenu: FC<TSubMenuProps> = ({ buttons }) => {
+const SubMenu: FC<TSubMenuProps> = ({ buttons, setIsMenuOpen }) => {
+  // useEffect(() => {
+  //   const toggleFn = () => setIsMenuOpen(false)
+  //   window.addEventListener('click', toggleFn)
+  //   return () => window.removeEventListener('click', toggleFn)
+  // }, [])
+
   // TODO: компонент для кнопки в SubMenu
   return (
-    <div className={styles.SubMenu}>
+    <div className={styles.sub_menu}>
       {buttons.map(button => (
         <SubMenuButton key={button.name} button={button}></SubMenuButton>
       ))}
