b3cda1a40d441a99524083626904815f015a9eb7
Woland-prj <nzimin93@gmail.com>
2024-01-12 01:03:39 +0300

    оптимизация рендеринга при помощи useCallback

--- src/hooks/useObjectsStyles.ts
+++ src/hooks/useObjectsStyles.ts
@@ -1,13 +1,6 @@
 import { ObjectType, TImage, TText, TVector } from '@/types/type'
+import { useCallback } from 'react'
 import { useStyles } from './useStyles'
-
-// export type TObjectStyle = {
-//   styleFn:
-//     | ((object: TText) => React.CSSProperties)
-//     | ((object: TVector) => React.CSSProperties)
-//     | ((object: TImage) => React.CSSProperties)
-//   params: unknown[]
-// }
 
 export const useObjectsStyles = (
   object: TText | TImage | TVector,
@@ -22,33 +15,42 @@ export const useObjectsStyles = (
   }
   switch (object.type) {
     case ObjectType.Text: {
-      const styleFn = (object: TText): React.CSSProperties => {
-        return {
-          ...baseStyles,
-          color: object.formatting.color,
-          fontSize: object.formatting.fontSize,
-          fontFamily: object.formatting.fontFamily,
-        }
-      }
       const params: unknown[] = [...baseParams, object.formatting]
+      const styleFn = useCallback(
+        (object: TText): React.CSSProperties => {
+          return {
+            ...baseStyles,
+            color: object.formatting.color,
+            fontSize: object.formatting.fontSize,
+            fontFamily: object.formatting.fontFamily,
+          }
+        },
+        [...params],
+      )
       return useStyles<TText>(params, object, styleFn)
     }
     case ObjectType.Vector: {
-      const styleFn = (object: TVector): React.CSSProperties => {
-        return {
-          ...baseStyles,
-        }
-      }
       const params: unknown[] = [...baseParams]
+      const styleFn = useCallback(
+        (object: TVector): React.CSSProperties => {
+          return {
+            ...baseStyles,
+          }
+        },
+        [...params],
+      )
       return useStyles<TVector>(params, object, styleFn)
     }
     case ObjectType.Image: {
-      const styleFn = (object: TImage): React.CSSProperties => {
-        return {
-          ...baseStyles,
-        }
-      }
       const params: unknown[] = [...baseParams]
+      const styleFn = useCallback(
+        (object: TImage): React.CSSProperties => {
+          return {
+            ...baseStyles,
+          }
+        },
+        [...params],
+      )
       return useStyles<TImage>(params, object, styleFn)
     }
   }
