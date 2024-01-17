cbfcedfe94537351a48dfb9564fc9b89039bc443
unknown <denkist555@gmail.com>
2024-01-17 15:31:50 +0300

    Нужно

--- src/components/ui/app_menu/actions_menu/TextInputButton.tsx
+++ src/components/ui/app_menu/actions_menu/TextInputButton.tsx
@@ -1,5 +1,6 @@
 import { FC, useState } from 'react'
 import styles from './ActionsMenu.module.css'
+import { useEditor } from '@/hooks/useEditor'
 
 type TextInputButtonProps = {
   type: string
@@ -14,7 +15,20 @@ export const TextInputButton: FC<TextInputButtonProps> = ({
     if (type == 'text') return 'Times New Roman'
     else return '14'
   }
+  const {setFontSize} = useAction()
+  const {activeSlideId} = useEditor()
   const [textValue, setTextValue] = useState(standardValue(type))
+  const saveText = () => {
+    if (type == 'number' && parseInt(textValue) != undefined) {
+      slides[activeSlideId].objects.forEach(obj => {
+        if (
+          selectedObjectsIds.find(selId => selId === obj.id) != undefined &&
+          obj.type === ObjectType.Vector
+        )
+          setObjectColorAction(obj.id, textValue, type)
+      })
+    }
+  }
   return (
     <div className={styles.input_button}>
       <div className={styles.image_button}></div>
